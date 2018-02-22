8c7ed89ddfd4c62f6a01b1ba516db432f3333ae0
CodeZhang <552146383@qq.com>
2018-02-22 13:42:18 +0800

    图片缩放

--- app/src/main/java/com/zs/project/ui/activity/ImageShowActivity.kt
+++ app/src/main/java/com/zs/project/ui/activity/ImageShowActivity.kt
@@ -1,11 +1,9 @@
 package com.zs.project.ui.activity
 
-import android.graphics.BitmapFactory
 import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
-import android.widget.RelativeLayout
 import com.zs.project.R
 import com.zs.project.base.BaseActivity
 import com.zs.project.util.ImageLoaderUtil
@@ -50,28 +48,6 @@ class ImageShowActivity : BaseActivity(){
         //创建一个photoview的一个attacher
         ImageLoaderUtil.displayImage(mImageUrl,iv_show_img)
     }
-
-    private fun displayImage() {
-        //想让图片宽是屏幕的宽度
-        val bitmap = BitmapFactory.decodeResource(resources, R.mipmap.default_img)
-        //测量
-        val options = BitmapFactory.Options()
-        options.inJustDecodeBounds = true//只测量
-        val height = bitmap.height
-        val width = bitmap.width
-        //再拿到屏幕的宽
-        val windowManager = windowManager
-        val display = windowManager.defaultDisplay
-        val screenWidth = display.width
-        //计算如果让照片是屏幕的宽，选要乘以多少？
-        scale = screenWidth / width
-        //这个时候。只需让图片的宽是屏幕的宽，高乘以比例
-        val displayHeight = height * scale//要显示的高，这样避免失真
-        //最终让图片按照宽是屏幕 高是等比例缩放的大小
-        val layoutParams = RelativeLayout.LayoutParams(screenWidth, displayHeight)
-        iv_show_img.layoutParams = layoutParams
-    }
-
 
     override fun onClick(p0: View?) {
     }
