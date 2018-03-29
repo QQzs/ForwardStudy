9d871a638291d0386d55d4da2b3c7f7baf18c3f1
CodeZhang <552146383@qq.com>
2018-03-29 22:03:05 +0800

    update

--- app/src/main/java/com/zs/project/app/AppStatusManager.java
+++ app/src/main/java/com/zs/project/app/AppStatusManager.java
@@ -5,7 +5,7 @@ package com.zs.project.app;
  * Date：2018年 02月 27日
  * Time：11:48
  * —————————————————————————————————————
- * About:
+ * About:activity manager
  * —————————————————————————————————————
  */
 
