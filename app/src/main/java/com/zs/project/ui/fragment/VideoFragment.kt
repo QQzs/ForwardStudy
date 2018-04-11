a3b38590b46a022be3f55550706ad6ead628bfd6
CodeZhang <552146383@qq.com>
2018-04-11 15:20:52 +0800

    2.0 视频播放

--- app/src/main/java/com/zs/project/ui/fragment/VideoFragment.kt
+++ app/src/main/java/com/zs/project/ui/fragment/VideoFragment.kt
@@ -5,9 +5,13 @@ import android.media.MediaMetadataRetriever
 import android.os.Bundle
 import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.util.TypedValue
+import android.view.MotionEvent
 import android.view.View
 import android.widget.ImageView
-import cn.jzvd.*
+import cn.jzvd.JZUserAction
+import cn.jzvd.JZVideoPlayer
+import cn.jzvd.JZVideoPlayerStandard
 import com.jcodecraeer.xrecyclerview.ProgressStyle
 import com.jcodecraeer.xrecyclerview.XRecyclerView
 import com.mcxtzhang.commonadapter.rv.CommonAdapter
@@ -23,6 +27,7 @@ import com.zs.project.request.RequestApi
 import com.zs.project.request.RequestHelper
 import com.zs.project.util.DateUtil
 import com.zs.project.util.ImageLoaderUtil
+import com.zs.project.util.LogUtil
 import com.zs.project.util.RecyclerViewUtil
 import com.zs.project.view.MultiStateView
 import io.reactivex.Observable
@@ -44,12 +49,14 @@ About: 视频Fragment
 class VideoFragment : BaseFragment(){
 
     var mPageNum : Int = 1
+    var mHolderPosition: Int? = null
 
     var mFragment: VideoFragment ?= null
     var mAdapter: CommonAdapter<ContentlistBean>? = null
+//    var mAdapter: VideoAdapter? = null
     var mData = mutableListOf<ContentlistBean>()
-
-    var mVideoUrl = "http://dn-chunyu.qbox.me/fwb/static/images/home/video/video_aboutCY_A.mp4"
+    var mVideoImage = arrayListOf(R.mipmap.iv_video_img1,R.mipmap.iv_video_img2,R.mipmap.iv_video_img3,R.mipmap.iv_video_img4,R.mipmap.iv_video_img5,R.mipmap.iv_video_img6,R.mipmap.iv_video_img7,R.mipmap.iv_video_img8,R.mipmap.iv_video_img9)
+    var mViewHolder : ViewHolder? = null
 
     val DATA_VIDEO : Int = 2000
 
@@ -72,50 +79,89 @@ class VideoFragment : BaseFragment(){
 
         mAdapter = object : CommonAdapter<ContentlistBean>(activity,mData,R.layout.item_video_layout){
             override fun convert(viewHolder: ViewHolder?, data: ContentlistBean?) {
-                viewHolder?.setText(R.id.tv_video_title,data?.text?.trim())
+//                viewHolder?.setText(R.id.tv_video_title,data?.text?.trim())
                 viewHolder?.setText(R.id.tv_voice_author,data?.name)
-                viewHolder?.setText(R.id.tv_video_time,DateUtil.friendlyTime(data?.create_time))
+                viewHolder?.setText(R.id.tv_video_time, DateUtil.friendlyTime(data?.create_time))
                 viewHolder?.setText(R.id.tv_voice_like,data?.love)
                 viewHolder?.setText(R.id.tv_video_follow,(Random().nextInt(200) + 100).toString())
 
                 var header = viewHolder?.getView<ImageView>(R.id.iv_video_header)
                 ImageLoaderUtil.loadAvatarImage(data?.profile_image,header)
                 var video = viewHolder?.getView<JZVideoPlayerStandard>(R.id.item_video_player)
-                video?.setUp(data?.video_uri, JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL, "")
-//                video?.setUp(mVideoUrl, JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL, "")
-                ImageLoaderUtil.displayLocalImage(R.mipmap.iv_video_img1,video?.thumbImageView)
+                video?.setUp(data?.video_uri, JZVideoPlayerStandard.SCREEN_WINDOW_LIST, data?.text?.trim())
+                video?.titleTextView?.setTextSize(TypedValue.COMPLEX_UNIT_SP,14f)
+                var position = if (viewHolder?.adapterPosition == null){
+                    0
+                }else{
+                    viewHolder?.adapterPosition !! -1
+                }
+                ImageLoaderUtil.displayLocalImage(mVideoImage[position % 9],video?.thumbImageView)
 
-//                JZVideoPlayerStandard.setJzUserAction { type, url, screen, objects ->
-//                    when(type){
-//                        JZUserAction.ON_CLICK_START_ICON ->{
-//                            viewHolder?.setVisible(R.id.tv_video_title,false)
-//                            viewHolder?.setVisible(R.id.tv_video_time,false)
-//                        }
-//                        JZUserAction.ON_CLICK_PAUSE ->{
-//                            viewHolder?.setVisible(R.id.tv_video_title,true)
-//                            viewHolder?.setVisible(R.id.tv_video_time,true)
-//                        }
-//                    }
-//                }
+                video?.startButton?.setOnTouchListener(object : View.OnTouchListener{
+                    override fun onTouch(view: View?, motionEvent: MotionEvent?): Boolean {
+                        if (motionEvent?.action == MotionEvent.ACTION_UP){
+                            mViewHolder = viewHolder
+                            mHolderPosition = position
+                            LogUtil.logShow("bbbbbbbbbb")
+                        }
+                        return false
+                    }
+
+                })
+                if (mHolderPosition != position){
+                    viewHolder?.setVisible(R.id.tv_video_title,true)
+                    viewHolder?.setVisible(R.id.tv_video_time,true)
+                }
 
             }
 
         }
+
+        JZVideoPlayerStandard.setJzUserAction { type, url, screen, objects ->
+
+            when(type){
+                JZUserAction.ON_CLICK_START_ICON , JZUserAction.ON_CLICK_RESUME ->{
+                    mViewHolder?.setVisible(R.id.tv_video_title,false)
+                    mViewHolder?.setVisible(R.id.tv_video_time,false)
+                    LogUtil.logShow("gone")
+                }
+                JZUserAction.ON_CLICK_PAUSE ->{
+                    mViewHolder?.setVisible(R.id.tv_video_title,true)
+//                    mViewHolder?.setVisible(R.id.tv_video_time,true)
+                    LogUtil.logShow("show")
+                }
+                JZUserAction.ON_AUTO_COMPLETE ->{
+                    LogUtil.logShow("AUTO_COMPLETE")
+                }
+                JZUserAction.ON_CLICK_START_AUTO_COMPLETE ->{
+                    LogUtil.logShow("CLICK_START_AUTO_COMPLETE")
+                }
+            }
+        }
+
         recycler_view?.setLoadingMoreProgressStyle(ProgressStyle.BallBeat)
         recycler_view?.setRefreshProgressStyle(ProgressStyle.BallSpinFadeLoader)
         RecyclerViewUtil.initNoDecoration(activity,recycler_view,mAdapter)
         recycler_view?.addOnChildAttachStateChangeListener(object : RecyclerView.OnChildAttachStateChangeListener{
             override fun onChildViewDetachedFromWindow(view: View?) {
+                // 滑出不可见 退出到小窗口播放
+                JZVideoPlayer.onChildViewDetachedFromWindow(view)
+
+//                不可见就停止播放视频
+//                val jzvd = view?.findViewById<JZVideoPlayerStandard>(R.id.item_video_player)
+//                if (jzvd != null && JZUtils.dataSourceObjectsContainsUri(jzvd!!.dataSourceObjects, JZMediaManager.getCurrentDataSource())) {
+//                    val currentJzvd = JZVideoPlayerManager.getCurrentJzvd()
+//                    if (currentJzvd != null && currentJzvd.currentScreen != JZVideoPlayer.SCREEN_WINDOW_FULLSCREEN) {
+//                        JZVideoPlayer.releaseAllVideos()
+//                        LogUtil.logShow("onChildViewAttachedToWindow")
+//                    }
+//                }
             }
 
             override fun onChildViewAttachedToWindow(view: View?) {
-                val jzvd = view?.findViewById<JZVideoPlayerStandard>(R.id.item_video_player)
-                if (jzvd != null && JZUtils.dataSourceObjectsContainsUri(jzvd!!.dataSourceObjects, JZMediaManager.getCurrentDataSource())) {
-                    val currentJzvd = JZVideoPlayerManager.getCurrentJzvd()
-                    if (currentJzvd != null && currentJzvd.currentScreen != JZVideoPlayer.SCREEN_WINDOW_FULLSCREEN) {
-                        JZVideoPlayer.releaseAllVideos()
-                    }
-                }
+                // 重新可见
+                JZVideoPlayer.onChildViewAttachedToWindow(view, R.id.item_video_player)
+
             }
 
         })
@@ -169,7 +215,7 @@ class VideoFragment : BaseFragment(){
             override fun onSuccess(response: VideoData?) {
 
                 mData = response?.showapi_res_body?.pagebean?.contentlist!!
-                mAdapter?.datas = mData
+                mAdapter?.setDatas(mData)
                 Log.d("My_Log","d = " + mData.toString())
 
                 if (mData.size == 0) {
@@ -181,7 +227,7 @@ class VideoFragment : BaseFragment(){
 
                 } else {
                     if (mPageNum == 1) {
-                        mAdapter?.datas = mData
+                        mAdapter?.setDatas(mData)
                         recycler_view?.refreshComplete()
                     } else {
                         mAdapter?.addDatas(mData)
@@ -189,7 +235,7 @@ class VideoFragment : BaseFragment(){
                     }
                 }
 
-                if (mAdapter?.datas?.size == 0){
+                if (mAdapter?.itemCount == 0){
                     multistate_view?.viewState = MultiStateView.VIEW_STATE_EMPTY
                 }else{
                     multistate_view?.viewState = MultiStateView.VIEW_STATE_CONTENT
